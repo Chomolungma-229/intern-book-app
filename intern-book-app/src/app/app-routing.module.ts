a7312db65a2333805235dd57309ac0bf053299b9
chomoranma229 <kokkororon871@gmail.com>
2022-04-08 12:26:14 +0900

    モックアップの作成

---
+++ intern-book-app/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+
+const routes : Routes = [
+  { path: '', redirectTo: '/pages', pathMatch: 'full' },
+  { path: 'pages', component: PagesComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [ RouterModule ],
+
+})
+export class AppRoutingModule { }
