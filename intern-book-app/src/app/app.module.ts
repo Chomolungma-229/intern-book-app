55823bbf8d4c5e9c5ddfc70b0e5f8e581fda50ab
chomoranma229 <kokkororon871@gmail.com>
2022-04-12 15:47:09 +0900

    pagesの修正

--- intern-book-app/src/app/app.module.ts
+++ intern-book-app/src/app/app.module.ts
@@ -6,17 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { PagesComponent } from './pages/pages.component';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +28,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatCardModule,
     MatInputModule,
     AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
