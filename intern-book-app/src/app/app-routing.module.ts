781305ebd82923d97c1d373be7c132786e8b071b
chomoranma229 <kokkororon871@gmail.com>
2022-04-14 15:36:20 +0900

    ルーティングのパスの修正

--- intern-book-app/src/app/app-routing.module.ts
+++ intern-book-app/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule,Routes } from '@angular/router';
 import { ListComponent } from './pages/list/list.component';
 
 const routes : Routes = [
-  { path: '', redirectTo: '/list', pathMatch: 'full' },
-  { path: 'list', component: ListComponent }
+  { path: '', redirectTo: '/pages/list', pathMatch: 'full' },
+  { path: 'pages/list', component: ListComponent }
 ];
 
 @NgModule({
