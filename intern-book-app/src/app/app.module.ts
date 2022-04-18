2a7b355bb02eff21726c0020633a3304913c159f
chomoranma229 <kokkororon871@gmail.com>
2022-04-18 13:05:16 +0900

    記述をわかりやすくする修正

--- intern-book-app/src/app/app.module.ts
+++ intern-book-app/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
-
+//pages
+import { ListComponent } from './pages/list/list.component';
+//components
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+//Angular Material
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import { ListComponent } from './pages/list/list.component';
-import {MatIconModule} from '@angular/material/icon';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
 
 
 @NgModule({
